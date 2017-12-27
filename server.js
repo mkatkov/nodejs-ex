5c996c0fbc9ebd479b70c6e39d4dc1bb0c268798
Mikhail Katkov <mikhail.katkov@gmail.com>
2017-12-27 14:18:45 +0200

    test1

--- server.js
+++ server.js
@@ -77,6 +77,13 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/abc', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+ 
+    res.send('abc Ok!');
+})
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
